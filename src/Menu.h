8ea8999442ddc8cc9fac76eacb9518e332adc515
Fernando <fernandofcaputo@gmail.com>
2020-09-12 22:04:56 -0300

    Terminado

---
+++ src/Menu.h
@@ -0,0 +1,19 @@
+/*
+ * TP1.h
+ *
+ *  Created on: 12 sep. 2020
+ *      Author: Fernando Caputo
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+/**
+*\ Imprime el menu del programa
+*\param numero1 Numero a ser mostrado en la posicion A
+*\param numero2 Numero a ser mostrado en la posicion B
+**/
+void getMenu(float numero1,float numero2);
+
+
+#endif /* MENU_H_ */
